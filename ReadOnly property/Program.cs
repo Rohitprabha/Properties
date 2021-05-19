b24becd828dce1f0008a4ffdc16251cb44af0674
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-19 18:31:53 +0530

    Add files via upload

---
+++ ReadOnly property/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Readonly_Property
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            //myObj.Name = "Ram";                   //error 'Person.Name' cannot be assigned to -- it is read only
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
