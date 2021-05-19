d3d526c997d33574d01f4deeb31af435160dc191
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-19 18:35:10 +0530

    Add files via upload

---
+++ WriteOnly Property/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WriteOnly_Property
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Ram";
+            //Console.WriteLine(myObj.Name);  //error 'Person.Name' cannot be used in this context because it lacks the get accessor
+        }
+    }
+}
