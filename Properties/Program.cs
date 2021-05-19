469ae14955e082e609b0ca5932d5e4cc80ebb7a0
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-19 18:30:02 +0530

    Add files via upload

---
+++ Properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Ram";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
